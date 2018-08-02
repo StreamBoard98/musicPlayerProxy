44014d1619f925ea48bd2cb4680b72efdcca08fd
ringo_sanchez <ringo196@gmail.com>
2018-08-02 14:47:34 -0700

    Update proxy server

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+ // module.exports = {
+ //    entry: {
+ //      vendor: ['styled-components'],
+ //      musicPlayer: '../musicPlayerService/client/src/index.js',
+ //      suggestedTracks: '../suggestedTracksService/client/src/index.jsx',
+ //      comments: '../commentsListAndSubmissionService/client/src/index.jsx',
+ //    },
+ //    output: {
+ //      filename: "[name].[chunkhash].bundle.js",
+ //      path: __dirname + "/dist"
+ //    },
+ //    optimization: {
+ //      splitChunks: {
+ //        chunks: "all"
+ //      }
+ //    },
+ //  }
